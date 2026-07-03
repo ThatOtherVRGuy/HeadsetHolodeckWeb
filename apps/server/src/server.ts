c6f19ca53f8e885b17424d0107ab728b3b54caeb
Dave Arendash <dave@qlcomp.com>
2026-07-02 22:53:57 -0700

    fix: load server env from repo root

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,10 +1,13 @@
 import dotenv from "dotenv";
+import { fileURLToPath } from "node:url";
 import { buildServer } from "./app.js";
 import { readServerEnv } from "./config/env.js";
 import { OpenAiTranscriptionClient } from "./openai/transcriptionClient.js";
 import { WorldLabsClient } from "./worldlabs/worldLabsClient.js";
 
-dotenv.config();
+dotenv.config({
+  path: fileURLToPath(new URL("../../../.env", import.meta.url))
+});
 
 const env = readServerEnv(process.env);
 const app = await buildServer({
